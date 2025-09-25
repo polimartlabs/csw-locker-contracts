e552a37ba6b7211960e8248d5e8daa3826a9013c
BowTiedRadone <bowtiedradone@gmail.com>
2025-09-25 21:10:33 +0300

    Fix, expand, prepare pox4 delegation extension for property-testing

--- tests/ext-delegate-stx-pox-4.test.ts
+++ tests/ext-delegate-stx-pox-4.test.ts
@@ -1,61 +1,196 @@
-import {
-  CoreNodeEventType,
-  cvToValue,
-  hexToBytes,
-  projectFactory,
-} from "@clarigen/core";
-import { filterEvents, txOk } from "@clarigen/test";
+import { CoreNodeEventType, cvToValue, hexToBytes } from "@clarigen/core";
+import { filterEvents } from "@clarigen/test";
 import { tx } from "@hirosystems/clarinet-sdk";
 import {
+  Cl,
   boolCV,
   bufferCV,
   principalCV,
   tupleCV,
   uintCV,
 } from "@stacks/transactions";
-import { describe, expect, test } from "vitest";
-import {
-  accounts,
-  deployments,
-  project,
-} from "../../clarigen/src/clarigen-types";
+import { describe, expect, it } from "vitest";
+import { deployments } from "../clarigen/src/clarigen-types";
 
-const { smartWalletEndpoint, extDelegateStxPox4 } = projectFactory(
-  project,
-  "simnet"
-);
+const accounts = simnet.getAccounts();
+const deployer = accounts.get("deployer")!;
+const poolAdmin = accounts.get("wallet_2")!;
 
-const delegationAmount = 100;
-
-const deployer = accounts.deployer.address;
-const poolAdmin = accounts.wallet_2.address;
+// General error codes
+const ERR_NOT_ENOUGH_BALANCE = 1;
 
 const smartWallet = deployments.smartWalletStandard.simnet;
 const delegateExtension = deployments.extDelegateStxPox4.simnet;
+const smartWalletEndpoint = deployments.smartWalletEndpoint.simnet;
+
 describe("Standard wallet with delegate-stx-pox-4 extension", () => {
-  test("that user can delegate and pool admin can lock", async () => {
-    const stxTransfer = tx.transferSTX(10000000000, smartWallet, deployer);
+  it("user can delegate using endpoint and fully funded smart wallet", () => {
+    const transferAmount = 100;
+    const stxTransfer = tx.transferSTX(transferAmount, smartWallet, deployer);
     simnet.mineBlock([stxTransfer]);
 
-    // delegate to pool admin
-    const response = txOk(
-      smartWalletEndpoint.delegateStx(
-        smartWallet,
-        delegateExtension,
-        delegationAmount,
-        poolAdmin
-      ),
+    const { result: delegateResult } = simnet.callPublicFn(
+      smartWalletEndpoint,
+      "delegate-stx",
+      [
+        Cl.principal(smartWallet),
+        Cl.principal(delegateExtension),
+        Cl.uint(transferAmount),
+        Cl.principal(poolAdmin),
+      ],
       deployer
     );
 
-    expect(response.result).toBeOk(boolCV(true));
+    expect(delegateResult).toBeOk(boolCV(true));
+  });
+
+  it("delegate call fails if smart wallet is underfunded", () => {
+    const transferAmount = 100;
+    const delegationAmount = transferAmount + 1;
+    const stxTransfer = tx.transferSTX(transferAmount, smartWallet, deployer);
+    simnet.mineBlock([stxTransfer]);
+
+    const { result: delegateResult } = simnet.callPublicFn(
+      smartWalletEndpoint,
+      "delegate-stx",
+      [
+        Cl.principal(smartWallet),
+        Cl.principal(delegateExtension),
+        Cl.uint(delegationAmount),
+        Cl.principal(poolAdmin),
+      ],
+      deployer
+    );
+
+    expect(delegateResult).toBeErr(Cl.uint(ERR_NOT_ENOUGH_BALANCE));
+  });
+
+  it("successful delegation call correctly prints the expected events", () => {
+    const transferAmount = 100;
+    const delegationAmount = 100;
+    const stxTransfer = tx.transferSTX(transferAmount, smartWallet, deployer);
+    simnet.mineBlock([stxTransfer]);
+
+    const { events: delegateEvents, result: delegateResponse } =
+      simnet.callPublicFn(
+        smartWalletEndpoint,
+        "delegate-stx",
+        [
+          Cl.principal(smartWallet),
+          Cl.principal(delegateExtension),
+          Cl.uint(delegationAmount),
+          Cl.principal(poolAdmin),
+        ],
+        deployer
+      );
+
+    expect(delegateResponse).toBeOk(boolCV(true));
+
+    // payload print event
+    const contractEvents = filterEvents(
+      delegateEvents,
+      CoreNodeEventType.ContractEvent
+    );
+
+    expect(contractEvents.length).toEqual(1);
+    const [payloadEvent] = contractEvents;
+    const payloadData = cvToValue<{
+      a: string;
+      payload: { extension: string };
+    }>(payloadEvent.data.value);
+    expect(payloadData.a).toEqual("extension-call");
+    expect(payloadData.payload.extension).toEqual(
+      deployments.extDelegateStxPox4.simnet
+    );
+
+    // extension call token events
+
+    // mint print event
+    const ectMintEvents = filterEvents(
+      delegateEvents,
+      CoreNodeEventType.FtMintEvent
+    );
+    expect(ectMintEvents.length).toEqual(1);
+    const [ectMintEvent] = ectMintEvents;
+    expect(ectMintEvent.data.amount).toEqual("1");
+
+    // burn print event
+    const ectBurnEvents = filterEvents(
+      delegateEvents,
+      CoreNodeEventType.FtBurnEvent
+    );
+    expect(ectBurnEvents.length).toEqual(1);
+    const [ectBurnEvent] = ectBurnEvents;
+    expect(ectBurnEvent.data.amount).toEqual("1");
+
+    // stx transfer print event from smart wallet to extension
+    const stxEvents = filterEvents(
+      delegateEvents,
+      CoreNodeEventType.StxTransferEvent
+    );
+    const [stxEvent] = stxEvents;
+    expect(stxEvent.data.amount).toEqual(delegationAmount.toString());
+    expect(stxEvent.data.sender).toEqual(smartWallet);
+    expect(stxEvent.data.recipient).toEqual(
+      deployments.extDelegateStxPox4.simnet
+    );
+  });
+
+  it("user can revoke an existing delegation using endpoint and fully funded smart wallet", () => {
+    const delegationAmount = 100;
+    const stxTransfer = tx.transferSTX(delegationAmount, smartWallet, deployer);
+    simnet.mineBlock([stxTransfer]);
+
+    simnet.callPublicFn(
+      smartWalletEndpoint,
+      "delegate-stx",
+      [
+        Cl.principal(smartWallet),
+        Cl.principal(delegateExtension),
+        Cl.uint(delegationAmount),
+        Cl.principal(poolAdmin),
+      ],
+      deployer
+    );
+
+    const { result: revokeResult } = simnet.callPublicFn(
+      smartWalletEndpoint,
+      "revoke-delegate-stx",
+      [Cl.principal(smartWallet), Cl.principal(delegateExtension)],
+      deployer
+    );
+
+    expect(revokeResult).toBeOk(boolCV(true));
+  });
+
+  it("user can delegate using smart wallet endpoint and pool admin can lock", () => {
+    const transferAmount = 100;
+    const delegationAmount = 100;
+    const stxTransfer = tx.transferSTX(transferAmount, smartWallet, deployer);
+    simnet.mineBlock([stxTransfer]);
+
+    const { events: delegateEvents, result: delegateResponse } =
+      simnet.callPublicFn(
+        smartWalletEndpoint,
+        "delegate-stx",
+        [
+          Cl.principal(smartWallet),
+          Cl.principal(delegateExtension),
+          Cl.uint(delegationAmount),
+          Cl.principal(poolAdmin),
+        ],
+        deployer
+      );
+
+    expect(delegateResponse).toBeOk(boolCV(true));
 
     // check for print event
     const printEvents = filterEvents(
-      response.events,
+      delegateEvents,
       CoreNodeEventType.ContractEvent
     );
-    expect(printEvents.length).toEqual(2);
+
+    expect(printEvents.length).toEqual(1);
     const [print] = printEvents;
     const printData = cvToValue<{
       a: string;
@@ -66,31 +201,39 @@ describe("Standard wallet with delegate-stx-pox-4 extension", () => {
       deployments.extDelegateStxPox4.simnet
     );
 
-    // extension call token event
-    const ectEvents = filterEvents(
-      response.events,
+    // extension call token events
+    // mint event
+    const ectMintEvents = filterEvents(
+      delegateEvents,
+      CoreNodeEventType.FtMintEvent
+    );
+    expect(ectMintEvents.length).toEqual(1);
+    const [ectMintEvent] = ectMintEvents;
+    expect(ectMintEvent.data.amount).toEqual("1");
+
+    // burn event
+    const ectBurnEvents = filterEvents(
+      delegateEvents,
       CoreNodeEventType.FtBurnEvent
     );
-    expect(ectEvents.length).toEqual(1);
+    expect(ectBurnEvents.length).toEqual(1);
+    const [ectBurnEvent] = ectBurnEvents;
+    expect(ectBurnEvent.data.amount).toEqual("1");
 
-    const [ectEvent] = ectEvents;
-    expect(ectEvent.data.amount).toEqual("1");
-
-    // check stx transfer event from smart wallet to extension
+    // stx transfer event from smart wallet to extension
     const stxEvents = filterEvents(
-      response.events,
+      delegateEvents,
       CoreNodeEventType.StxTransferEvent
     );
     const [stxEvent] = stxEvents;
     expect(stxEvent.data.amount).toEqual(delegationAmount.toString());
-
     expect(stxEvent.data.sender).toEqual(smartWallet);
     expect(stxEvent.data.recipient).toEqual(
       deployments.extDelegateStxPox4.simnet
     );
 
-    // let pool admin lock
-    const lockResponse = simnet.callPublicFn(
+    // pool admin locks
+    const { result: lockResult } = simnet.callPublicFn(
       "SP000000000000000000002Q6VF78.pox-4",
       "delegate-stack-stx",
       [
@@ -102,13 +245,13 @@ describe("Standard wallet with delegate-stx-pox-4 extension", () => {
             hexToBytes("b0b75f408a29c271d107e05d614d0ff439813d02")
           ),
         }),
-        uintCV(100),
+        uintCV(delegationAmount),
         uintCV(1),
       ],
       poolAdmin
     );
 
-    expect(lockResponse.result).toBeOk(
+    expect(lockResult).toBeOk(
       tupleCV({
         "lock-amount": uintCV(delegationAmount),
         stacker: principalCV(deployments.extDelegateStxPox4.simnet),
